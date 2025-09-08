137dc38724b9ff48265f2b6ed30f0abee6ef940e
Vivek Naik <vnaik@irobot.com>
2025-09-08 09:17:52 +0530

    Started velocities for the atoms

--- src/atoms.rs
+++ src/atoms.rs
@@ -1,11 +1,13 @@
-use na::Matrix3xX;
+use na::{DVector, Matrix3xX, Vector3};
+use rand_distr::{Distribution, Normal};
+use crate::constants::KB_KJPERMOLEKELVIN;
 
 pub struct Atoms {
     n_atoms: usize,
     positions: Matrix3xX<f64>,
     velocities: Matrix3xX<f64>,
     forces: Matrix3xX<f64>,
-    masses: Vec<f64>,
+    masses: DVector<f64>,
     types: Vec<String>,
 }
 
@@ -16,13 +18,70 @@ impl Atoms {
             positions: Matrix3xX::zeros(n_atoms),
             velocities: Matrix3xX::zeros(n_atoms),
             forces: Matrix3xX::zeros(n_atoms),
-            masses: vec![1.0; n_atoms],
+            masses: DVector::from_element(n_atoms, 1.0),
             types: (0..n_atoms).map(|_| String::with_capacity(3)).collect(),
         }
     }
 
-    pub fn new() -> Self {
+    pub fn new(temperature: f64) -> Self {
         let n_atoms: usize = 10;
-        Self::new_zeroes(n_atoms)
+        let mut this = Self::new_zeroes(n_atoms);
+        this.start_velocities(temperature);
+        this
+    }
+
+    pub fn start_velocities(&mut self, temperature: f64) {
+        self.initialise_velocities(temperature);
+        self.remove_drift();
+        self.rescale_to_temperature(temperature);
+    }
+
+    fn initialise_velocities(&mut self, temperature: f64) {
+        let mut rng = rand::rng();
+
+        let mut sigma: f64;
+        let mut normal: Normal<f64>;
+        
+        for (i, a_mass) in self.masses.iter().enumerate() {
+            sigma = (KB_KJPERMOLEKELVIN * temperature / a_mass).sqrt();
+            normal = Normal::new(0.0, sigma).unwrap();
+
+            self.velocities[(0, i)] = normal.sample(&mut rng);
+            self.velocities[(1, i)] = normal.sample(&mut rng);
+            self.velocities[(2, i)] = normal.sample(&mut rng);
+        }        
+    }
+
+    fn remove_drift(&mut self) {
+        let total_mass: f64 = self.masses.sum();
+        // (3×n)⋅(n×1)=(3×1)
+        let total_momentum: Vector3<f64> = &self.velocities * &self.masses;
+
+        let velocity_cm = total_momentum / total_mass;
+
+        let ones = DVector::from_element(self.n_atoms, 1.0);
+        // (3 × 1) * (1 × n) = (3 × n)
+        self.velocities -= velocity_cm * ones.transpose();
+    }
+
+    fn rescale_to_temperature(&mut self, temperature: f64) {
+        let current_temerature = self.current_temerature();
+
+        let lambda = (temperature / current_temerature).sqrt();
+
+        self.velocities *= lambda;
+    }
+
+    pub fn kinetic_energy(&self) -> f64 {
+        self.velocities
+            .column_iter()
+            .zip(self.masses.iter())
+            .map(|(velocity, &mass)| 0.5 * mass * velocity.norm_squared())
+            .sum()
+    }
+
+    pub fn current_temerature(&self) -> f64 {
+        let kinetic_energy = self.kinetic_energy();
+        (2.0 * kinetic_energy ) / (3.0 * self.n_atoms as f64 * KB_KJPERMOLEKELVIN)
     }
 }
