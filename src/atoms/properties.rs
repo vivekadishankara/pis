cecb9366ab48a5af3f0035b0895249b92f8a9942
Vivek Naik <vnaik@irobot.com>
2025-09-09 13:12:45 +0530

    Started verlet simulation

--- src/atoms/properties.rs
+++ src/atoms/properties.rs
@@ -1,3 +1,5 @@
+use na::Matrix3xX;
+
 use crate::atoms::new::Atoms;
 use crate::constants::KB_KJPERMOLEKELVIN;
 
@@ -11,8 +13,16 @@ impl Atoms {
             .sum()
     }
 
-    pub fn current_temerature(&self) -> f64 {
-        let kinetic_energy = self.kinetic_energy();
+    pub fn current_temerature(&self, kinetic_energy: f64) -> f64 {
         (2.0 * kinetic_energy ) / (3.0 * self.n_atoms as f64 * KB_KJPERMOLEKELVIN)
+    }
+
+    pub fn current_acceleration(&self) -> Matrix3xX<f64> {
+        let mut acceleration = Matrix3xX::zeros(self.n_atoms);
+        for i in 0..self.n_atoms {
+            let mut a_i = acceleration.column_mut(i);
+            a_i += self.forces.column(i) / self.masses[i]; 
+        }
+        acceleration
     }
 }
