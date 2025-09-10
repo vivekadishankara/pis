0519b624c8dd6718d59c02310e181a667d912a85
Vivek Naik <vnaik@irobot.com>
2025-09-10 06:20:05 +0530

    Started building the args parser

---
+++ src/args_parser.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+pub struct Args {
+    #[arg(short, long, default_value_t = String::from("data.txt"))]
+    pub infile: String,
+
+    #[arg(short, long, default_value_t = 50)]
+    pub steps: usize,
+
+    #[arg(short, long, default_value_t = 0.001)]
+    pub timestep: f64,
+}
