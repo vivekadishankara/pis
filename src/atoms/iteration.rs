2de9d131ea94f8380570ce3aba817e18d5559d45
Vivek Naik <vnaik@irobot.com>
2025-09-14 05:53:31 +0530

    Started verlet list implementation

--- src/atoms/iteration.rs
+++ src/atoms/iteration.rs
@@ -1,9 +1,136 @@
+use std::collections::HashSet;
 use na::{DVector, Matrix3xX};
 
-use crate::{atoms::new::Atoms, writers::dump_traj::DumpTraj};
+use crate::{atoms::{neighbour_list::FORWARD_NEIGHBOUR_OFFSETS, new::Atoms}, writers::dump_traj::DumpTraj};
 
 
 impl Atoms {
+    pub fn compute_potential_and_forces_n_list(&mut self) -> f64 {
+        let max_rcut = self.potential_manager.max_rcut();
+        let (nx, ny, nz) = self.divide_into_cells(max_rcut);
+        let cells = self.rcut_cells(nx, ny, nz);
+
+        let mut neighbour_cells: HashSet<usize> = HashSet::new();
+        let mut potential_energy: f64 = 0.0;
+
+        for cx_i in 0..nx {
+            for cy_i in 0..ny {
+                for cz_i in 0..nz {
+                    let current_cell = Self::cell_index(cx_i, cy_i, cz_i, nx, ny);
+                    let current_cell_atoms = &cells[current_cell];
+
+                    for offset in FORWARD_NEIGHBOUR_OFFSETS.iter() {
+                        let cx_j = (cx_i as isize + offset.dx).rem_euclid(nx as isize) as usize;
+                        let cy_j = (cy_i as isize + offset.dy).rem_euclid(ny as isize) as usize;
+                        let cz_j = (cz_i as isize + offset.dz).rem_euclid(nz as isize) as usize;
+
+                        let a_neighbour_cell = Self::cell_index(cx_j, cy_j, cz_j, nx, ny);
+                        if neighbour_cells.contains(&a_neighbour_cell) { continue; }
+                        neighbour_cells.insert(a_neighbour_cell);
+
+                        let neighbour_cell_atoms = &cells[a_neighbour_cell];
+
+                        for &i in current_cell_atoms.iter() {
+                            for &j in neighbour_cell_atoms.iter() {
+                                if current_cell == a_neighbour_cell && i<=j { continue; }
+                                let mut rij = self.positions.column(j) - self.positions.column(i);
+                                self.sim_box.apply_boundary_conditions_dis(&mut rij);
+
+                                let potential = match self.get_potential_ij(i,j) {
+                                    Some(pot) => pot,
+                                    None => {
+                                        println!("During force calculation between {} and {} atoms, potential was missing", i + 1, j + 1);
+                                        continue;
+                                    }                                            
+                                };
+
+                                if rij.norm() > potential.get_rcut() { continue; }
+                                let (uij, force_ij) = potential.compute_potetial(&rij);
+                                potential_energy += uij;
+                                {
+                                    let mut fi = self.forces.column_mut(i);
+                                    fi -= force_ij;
+
+                                    let mut fj = self.forces.column_mut(j);
+                                    fj += force_ij;
+                                }
+                            }
+                        }
+                    }
+                    neighbour_cells.clear();
+                }
+            }
+        }
+        potential_energy
+    }
+
+    #[allow(dead_code)]
+    pub fn compute_potential_and_forces_n_list1 (&mut self) -> f64 {
+        let max_rcut = self.potential_manager.max_rcut();
+
+        let (nx, ny, nz) = self.divide_into_cells(max_rcut);
+        let shift = [-1, 0, 1];
+        let cells = self.rcut_cells(nx, ny, nz);
+
+        let mut neighbour_cells: HashSet<usize> = HashSet::new();
+        let mut potential_energy: f64 = 0.0;
+
+        for cx_i in 0..nx {
+            for cy_i in 0..ny {
+                for cz_i in 0..nz {
+                    let current_cell = Self::cell_index(cx_i, cy_i, cz_i, nx, ny);
+                    let current_cell_atoms = &cells[current_cell];
+
+                    for dx in &shift {
+                        for dy in &shift {
+                            for dz in &shift {
+                                let cx_j = (cx_i as isize + dx).rem_euclid(nx as isize) as usize;
+                                let cy_j = (cy_i as isize + dy).rem_euclid(ny as isize) as usize;
+                                let cz_j = (cz_i as isize + dz).rem_euclid(nz as isize) as usize;
+
+                                let a_neighbour_cell = Self::cell_index(cx_j, cy_j, cz_j, nx, ny);
+                                if neighbour_cells.contains(&a_neighbour_cell) { continue; }
+                                neighbour_cells.insert(a_neighbour_cell);
+
+                                let neighbour_cell_atoms = &cells[a_neighbour_cell];
+
+                                for &i in current_cell_atoms.iter() {
+                                    for &j in neighbour_cell_atoms.iter() {
+                                        if i <= j { continue; }
+                                        let mut rij = self.positions.column(j) - self.positions.column(i);
+                                        self.sim_box.apply_boundary_conditions_dis(&mut rij);
+
+                                        let potential = match self.get_potential_ij(i,j) {
+                                            Some(pot) => pot,
+                                            None => {
+                                                println!("During force calculation between {} and {} atoms, potential was missing", i + 1, j + 1);
+                                                continue;
+                                            }                                            
+                                        };
+
+                                        if rij.norm() > potential.get_rcut() { continue; }
+                                        let (uij, force_ij) = potential.compute_potetial(&rij);
+                                        potential_energy += uij;
+                                        {
+                                            let mut fi = self.forces.column_mut(i);
+                                            fi -= force_ij;
+
+                                            let mut fj = self.forces.column_mut(j);
+                                            fj += force_ij;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    neighbour_cells.clear();
+                }
+            }
+        }
+        potential_energy
+    }
+
+    #[allow(dead_code)]
     pub fn compute_potential_and_forces(&mut self) -> f64 {
         let mut potential_energy: f64 = 0.0;
         for i in 0..self.n_atoms {
@@ -50,7 +177,7 @@ impl Atoms {
         
         self.forces = Matrix3xX::zeros(self.n_atoms);
 
-        let potential_energy = self.compute_potential_and_forces();
+        let potential_energy = self.compute_potential_and_forces_n_list();
 
         let a_tdt = self.current_acceleration();
 
@@ -68,14 +195,14 @@ impl Atoms {
         let mut potential_energies: DVector<f64> = DVector::zeros(time_steps + 1);
         let mut dumper = DumpTraj::new(dump_path).expect("Failed to create dump file");
         dumper.write_step(self, 0).expect("Failed to write step");
-        let first_potential = self.compute_potential_and_forces();
+        let first_potential = self.compute_potential_and_forces_n_list();
         potential_energies[0] = first_potential;
-        println!("{}", 0);
+        println!("{} {}", 0, first_potential);
         for i in 0..time_steps {
             let step_potential = self.verlet_step(dt);
             potential_energies[i + 1] = step_potential;
             dumper.write_step(self, i + 1).expect("Failed to write step");
-            println!("{}", i + 1);
+            println!("{} {}", i + 1, step_potential);
         }
         potential_energies
     }
