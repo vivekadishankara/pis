d7d4dc8e9d6b5753502d798f82475f999749c0fa
Vivek <vivek.adishankara@gmail.com>
2025-11-02 17:08:16 +0530

    Removed other arguments for the args_parser

--- src/args_parser.rs
+++ src/args_parser.rs
@@ -3,18 +3,6 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(author, version, about)]
 pub struct Args {
-    #[arg(short, long, default_value_t = String::from("data.txt"))]
+    #[arg(short, long, default_value_t = String::from("input.pis"))]
     pub infile: String,
-
-    #[arg(short, long, default_value_t = 50)]
-    pub steps: usize,
-
-    #[arg(short, long, default_value_t = 0.001)]
-    pub timestep: f64,
-
-    #[arg(short = 'T', long, default_value_t = 300.0)]
-    pub temperature: f64,
-
-    #[arg(short, long, default_value_t = String::from("dump.lammpstrj"))]
-    pub dump_path: String,
 }
